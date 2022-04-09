d6786c7bf9db892581c93ef4b6f47221001f5c87
priyanshuSharma-WebDev <priyanshushrama709@gmail.com>
2022-04-09 23:00:28 +0530

    put some changes

--- src/sidebar.jsx
+++ src/sidebar.jsx
@@ -11,15 +11,24 @@ function Sidebar() {
         <p>🙇</p>
         <p>Welcome Sir/Madam</p>
       </h2>
+      {/* <form className="oauth__form">
+          <div className="oauth__form__fields">
+            <label className='custom-file-upload' htmlFor="file">
+              <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" viewBox="0 0 24 24" fill="none" stroke="#7f7f7f" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-upload-cloud"><polyline points="16 16 12 12 8 16"></polyline><line x1="12" y1="12" x2="12" y2="21"></line><path d="M20.39 18.39A5 5 0 0 0 18 9h-1.26A8 8 0 1 0 3 16.3"></path><polyline points="16 16 12 12 8 16"></polyline></svg>
+              <p className='file-msg'>Choose a file</p>
+              <input type="file" id="file" />
+            </label>
+            <label htmlFor="name"><p>Name</p><input id="name" placeholder='Name' /></label>
+            <label htmlFor="name"><p>Name</p><input id="name" placeholder='Name' /></label>
+            <label htmlFor="name"><p>Name</p><input id="name" placeholder='Name' /></label>
+          </div>
+      </form> */}
       <div className="Vision__board">
         <img className="google__logo" src={GoogleLogo} />
         <div className="oauth__box">
           <p className="oauth" contentEditable="true">This year, I will get into Google India.</p>
           <span contentEditable="true">&ldquo;Priyanshu Sharma&rdquo;</span>
         </div>
-      </div>
-      <div className='description'>
-        Click for change text in the box above.
       </div>
     </>
   )
